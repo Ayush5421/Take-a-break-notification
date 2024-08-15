8ab541243b216ebea467abea34eb89ce47bd1976
Ayush Mukherjee <111524872+Ayush5421@users.noreply.github.com>
2024-08-15 17:40:12 +0530

    Add files via upload

---
+++ notify.py
@@ -0,0 +1,9 @@
+from plyer import notification
+import time
+if __name__ == '__main__':
+    while True:
+        notification.notify(
+            title="Alarm Clock",
+            message="wake up at 7'o clock",
+            timeout=5)
+        time.sleep(20)
